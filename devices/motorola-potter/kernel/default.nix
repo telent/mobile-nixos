fa2e7fbf6f134a96dc0fdd9c2ea4c4a326d5c4c2
Daniel Barlow <dan@telent.net>
2021-06-11 09:36:52 +0100

    switch to mainline-ish kernel
    initial config generated from
    ( cd arch/arm64/configs/ && cat defconfig msm8953.config motorola-potter.config )
    and with a few CONFIG_ARCH_* options removed.

--- devices/motorola-potter/kernel/default.nix
+++ devices/motorola-potter/kernel/default.nix
@@ -6,27 +6,28 @@
 }:
 
 
-mobile-nixos.kernel-builder-gcc6 {
-  version = "3.18.140";
+mobile-nixos.kernel-builder {
+  version = "5.13.0-rc4";
   configfile = ./config.aarch64;
 
   src = fetchFromGitHub {
-    owner = "boulzordev";
-    repo = "android_kernel_motorola_msm8953";
-    rev = "32b6f05688ff51e54958a8e97df24cd63a62b880";
-    sha256 = "0r0xkf6cq60bvckn311k43yxrm0qjp9psi6g3jfc6l5z4k549glm";
+    owner = "msm8953-mainline";
+    repo = "linux";
+    name = "kernel-postmarketos";
+    rev = "c9d3de2";
+    sha256 = "15899m43q70nlwyb06px2qq41jmz1w5zrir6p718fcrwfpay81p4";
   };
 
   patches = [
-    ./04_fix_camera_msm_isp.patch
-    ./99_framebuffer.patch
-    ./0001-Allow-building-WCD9335_CODEC-without-REGMAP_ALLOW_WR.patch
-    ./0005-Allow-building-with-sound-disabled.patch
-    ./0007-Coalesce-identical-device-trees.patch
-    ./0008-Notify-clients-when-FB-opened.patch
+    # ./04_fix_camera_msm_isp.patch
+    # ./99_framebuffer.patch
+    # ./0001-Allow-building-WCD9335_CODEC-without-REGMAP_ALLOW_WR.patch
+    # ./0005-Allow-building-with-sound-disabled.patch
+    # ./0007-Coalesce-identical-device-trees.patch
+    # ./0008-Notify-clients-when-FB-opened.patch
   ];
 
-  enableRemovingWerror = true;
-  isModular = false;
+#  enableRemovingWerror = true;
+  isModular = true;
   isQcdt = true;
 }
